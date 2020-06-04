d66814292aa5ed2153f731e363476fee806b5536
kazuki1225 <66419793+kazuki1225@users.noreply.github.com>
2020-06-04 21:48:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,11 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-
+@client.event
+async def on_ready(): # botが起動したときに動作する処理
+    print('ログインしました')
+    await client.change_presence(activity=discord.Game(name="with discord.py", type=1))
+    
 bot.run(token)
+
+
