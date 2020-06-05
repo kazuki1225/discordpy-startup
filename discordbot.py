59ec0ce686d61c5bdec90e4c610446cbde624c48
kazuki1225 <66419793+kazuki1225@users.noreply.github.com>
2020-06-05 17:59:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,12 +23,5 @@ async def おはよう(ctx):
 
 bot.run(token)
 
-global voich
-# 接続
-if message.content.startswith('connect'):
-    voich = await discord.VoiceChannel.connect(message.author.voice.channel)
-# 切断
-if message.content.startswith('discon'):
-    await voich.disconnect()
     
     
