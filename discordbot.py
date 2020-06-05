e521ec0eb8d91f9d3cd406e8bfd0ee796e7d2573
kazuki1225 <66419793+kazuki1225@users.noreply.github.com>
2020-06-05 16:15:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,21 +22,3 @@ async def おはよう(ctx):
     await ctx.send('おっはー！')         
 
 bot.run(token)
-
-# 返信する非同期関数を定義
-async def reply(message):
-    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-    await message.channel.send(reply) # 返信メッセージを送信
-
-# 発言時に実行されるイベントハンドラを定義
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        await reply(message) # 返信する非同期関数を実行
-
-@client.event
-client.on('ready', message =>
-{
-  client.user.setPresence({ game: { name: 'メンテナンス中' } });  
-  console.log('bot is ready!');
-});
