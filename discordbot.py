427dcb3d4d1e27d1c739a42cf937d8bebb63c3a6
kazuki1225 <66419793+kazuki1225@users.noreply.github.com>
2020-06-05 12:01:09 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -23,4 +23,8 @@ async def おはよう(ctx):
 
 bot.run(token)
 
+async def reply(message):
+    reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
+    await message.channel.send(reply) # 返信メッセージを送信
+
 
